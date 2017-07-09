1fd2aadfbbe3a0e0849486df8efcbfb41b442b86
Vic Lau <xingyulliiuu@gmail.com>
2017-07-09 15:10:37 +0800

    Implemented sign up.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to projects_path, notice: 'You have signed up successfully.'
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,
+                                 # :email,
+                                 :password,
+                                 :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   resources :projects do
     resources :tickets
   end
+
+  resources :users
+
 end
