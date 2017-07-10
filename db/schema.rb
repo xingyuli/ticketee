073caf2916f8d56a61d9db6759d74e95cf49f5c3
Vic Lau <xingyulliiuu@gmail.com>
2017-07-10 22:52:40 +0800

    fix db

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20170709133713) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: true
+    t.boolean "admin", default: false
   end
 
 end
