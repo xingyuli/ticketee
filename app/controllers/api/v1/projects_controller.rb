3ea0c0e08d46ccbb89c081db481065832752c1c5
Vic Lau <xingyulliiuu@gmail.com>
2017-08-05 22:06:26 +0800

    Restricting projects API show to only users who have permission to view a project

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::ProjectsController < Api::V1::BaseController
 
   before_action :authorize_admin!, except: [ :index, :show ]
+  before_action :set_project, only: [:show]
 
   def index
     respond_with Project.for(current_user).all
   end
 
   def show
-    @project = Project.find(params[:id])
     respond_with @project, methods: 'last_ticket'
   end
 
@@ -24,6 +24,15 @@ class Api::V1::ProjectsController < Api::V1::BaseController
 
   def project_params
     params.require(:project).permit(:name, :description)
+  end
+
+  def set_project
+    begin
+      @project = Project.for(current_user).find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      error = { error: 'The project you were looking for could not be found.' }
+      render params[:format].to_sym => error, status: 404
+    end
   end
 
 end
--- spec/api/v1/project_errors_spec.rb
+++ spec/api/v1/project_errors_spec.rb
@@ -15,6 +15,14 @@ RSpec.describe 'Project API errors', type: :api do
       expect(last_response.status).to eql(401)
       expect(Project.find_by_name('Ticketee')).to be_nil
     end
+
+    it 'cannot view projects they do not have access to' do
+      project = FactoryGirl.create(:project)
+      get "/api/v1/projects/#{project.id}.json", token: user.authentication_token
+      error = { error: 'The project you were looking for could not be found.' }
+      expect(last_response.status).to eql(404)
+      expect(last_response.body).to eql(error.to_json)
+    end
   end
 
 end
